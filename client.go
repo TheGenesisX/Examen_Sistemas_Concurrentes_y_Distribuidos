91971b35c977e5f073c0e4c8fe22d46aadadadd4
Alejandro Silva <jose.silva5468@alumnos.udg.mx>
2020-12-03 20:35:04 -0600

    Cambios en la manera de enviar los mensajes del cliente al servidor; tengo la idea de que me será más útil así para regresarlos desde el servidor hacia el resto.

--- client.go
+++ client.go
@@ -8,42 +8,63 @@ import (
 	"os"
 )
 
-var canal chan string = make(chan string)
+var login chan string = make(chan string)
 
-func cliente(canal chan string) {
+// User ...
+type User struct {
+	Nombre  string
+	Mensaje string
+}
+
+var messageChan chan User = make(chan User)
+
+func cliente(login chan string, messageChan chan User) {
 	cli, err := net.Dial("tcp", ":9999")
 	if err != nil {
 		fmt.Sprintln(err)
 		return
 	}
+	defer cli.Close()
 
 	for {
-		mensaje := <-canal
-		err = gob.NewEncoder(cli).Encode(mensaje)
-		if err != nil {
-			fmt.Sprintln(err)
+		select {
+		case loginMessage := <-login:
+			err = gob.NewEncoder(cli).Encode(loginMessage)
+			if err != nil {
+				fmt.Sprintln(err)
+				return
+			}
+
+		case newMessage := <-messageChan:
+			chatMessage := newMessage.Nombre + ": " + newMessage.Mensaje
+			err = gob.NewEncoder(cli).Encode(chatMessage)
+			if err != nil {
+				fmt.Sprintln(err)
+				return
+			}
 		}
 	}
-	// cli.Close()
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var opc uint64
+	var newUser User
 
-	go cliente(canal)
+	go cliente(login, messageChan)
 
 	fmt.Print("Username: ")
 	scanner.Scan()
 	nombre := scanner.Text()
+	newUser.Nombre = nombre
 
-	canal <- "Se ha conectado: " + nombre
+	login <- "Se ha conectado: " + nombre
 
 	for {
 		fmt.Println("1) Enviar mensaje")
 		fmt.Println("2) Enviar documento")
 		fmt.Println("3) Mostrar chat")
-		fmt.Println("4) Salir")
+		fmt.Println("0) Salir")
 		fmt.Print("Opcion: ")
 		fmt.Scanln(&opc)
 
@@ -52,8 +73,13 @@ func main() {
 			fmt.Print("Mensaje: ")
 			scanner.Scan()
 			mensaje := scanner.Text()
+			newUser.Mensaje = mensaje
 
-			canal <- nombre + ": " + mensaje
+			messageChan <- newUser
+		case 0:
+			return
+		default:
+			fmt.Println("Opcion no valida.")
 		}
 	}
 
